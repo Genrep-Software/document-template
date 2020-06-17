02015b1fc1b350ea2fb5caf58cdc329337daa6cc
Jacob Strieb <jacobstrieb@gmail.com>
2020-06-17 01:20:17 -0400

    Update compilation script

--- compile.sh
+++ compile.sh
@@ -6,14 +6,17 @@
 
 set -e
 
+# XXX: Adjust below if not running in Windows Subsystem for Linux with native
+# Windows Pandoc and pdfLaTeX
 PANDOC=pandoc.exe
+PDFLATEX=pdflatex.exe
 
 # Output TeX file
 $PANDOC \
   --template=template.tex \
   README.md \
-  -o README.tex
+  --output README.tex
 
 # Compile LaTeX to PDF -- do it twice for TOC update purposes
-pdflatex.exe README.tex
-pdflatex.exe README.tex
+$PDFLATEX README.tex
+$PDFLATEX README.tex
