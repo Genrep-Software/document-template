1d3253228474e14cda675217e96d55fe492ef6da
Jacob Strieb <jacobstrieb@gmail.com>
2020-06-17 10:27:15 -0400

    Update compilation to take non-Markdown metadata

--- compile.sh
+++ compile.sh
@@ -17,17 +17,40 @@ if [ -n "$1" ]; then
   INFILE="$1"
 else
   echo "Please specify an input file to convert!"
-  echo "Usage: $0 <infile>"
+  echo "Usage: $0 <infile.md>"
+  echo "   or  $0 <infile.*> \"<title>\" \"<author>\" \"<date>\""
   echo "Defaulting to \"README.md\"..."
 fi
 # Strip everything from the last period to the end of the string (inclusive)
 OUTFILE="$(echo $INFILE | sed 's/\(.*\)\.[^\.]*$/\1/')"".tex"
 
-# Output TeX file
-$PANDOC \
-  --template=template.tex \
-  "$INFILE" \
-  --output "$OUTFILE"
+# Get additional arguments for metadata if not Markdown based on extracting the
+# file extension
+if echo $INFILE | sed 's/.*\.\([^\.]*\)$/\1/' | grep -i md; then
+  # Assume there is metadata in the file if it is Markdown
+  # Output TeX file
+  $PANDOC \
+    --template=template.tex \
+    "$INFILE" \
+    --output "$OUTFILE"
+else
+  # If not Markdown, look for title, author, and date arguments (respectively)
+  if [ "$#" -eq 4 ]; then
+    # Output TeX file
+    $PANDOC \
+      --template=template.tex \
+      --metadata title:"$2" \
+      --metadata author:"$3" \
+      --metadata date:"$4" \
+      "$INFILE" \
+      --output "$OUTFILE"
+  else
+    echo "Please specify the title, author, and date in quotes!"
+    echo "Usage: $0 <infile.md>"
+    echo "   or  $0 <infile.*> \"<title>\" \"<author>\" \"<date>\""
+    exit
+  fi
+fi
 
 # Compile LaTeX to PDF -- do it twice for TOC update purposes
 $PDFLATEX "$OUTFILE"
